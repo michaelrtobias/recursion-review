00c1115147a75790db8e692b2d920a0a9b5dae1f
Michael Tobias <michaelremitobias@gmail.com>
2020-08-17 15:16:21 -0700

    "Finished stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var typeofObject = typeof obj;
+
+  if (typeofObject === 'number') {
+    return '' + obj + '';
+  } else if (obj === null) {
+    return '' + null + '';
+  } else if (typeofObject === 'boolean') {
+    return '' + obj + '';
+  } else if (typeofObject === 'string') {
+    return '"' + obj + '"';
+  } else if (typeofObject === 'object') {
+    if (Array.isArray(obj)) {
+
+      var result = '[';
+      for (var i = 0; i < obj.length; i++) {
+        result += stringifyJSON(obj[i]);
+        if (i !== obj.length - 1) {
+          result += ',';
+        }
+      }
+      result += ']';
+      return result;
+    } else {
+      var result = '{';
+      for (var key in obj) {
+        if (typeof obj[key] === 'function' || obj[key] === undefined ) {
+          break;
+        }
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        if (key !== Object.keys(obj).pop()) {
+          result += ',';
+        }
+
+      }
+
+      result += '}';
+      return result;
+    }
+
+  } else if (typeofObject === 'function') {
+    return '';
+  } else if (typeofObject === undefined) {
+    return {};
+  }
+
+
 };
